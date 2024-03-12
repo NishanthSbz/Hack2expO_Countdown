486e122a09b3c3fd4532144633368c7d4ac67bb7
NishanthSbz <nishanthsbz05@gmail.com>
2024-03-13 03:49:14 +0530

    bug fixed again

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,67 +175,67 @@ import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 
 
 
-// const btn = document.getElementById("startTimerButton");
-// const countdownText = document.getElementById("countdown-text");
+const btn = document.getElementById("startTimerButton");
+const countdownText = document.getElementById("countdown-text");
 
-// function countdownAndConfetti() {
-//     // Initialize countdown value
-//     let count = 3;
+function countdownAndConfetti() {
+    // Initialize countdown value
+    let count = 3;
 
-//     // Display countdown in the button
-//     btn.textContent = count;
+    // Display countdown in the button
+    btn.textContent = count;
 
-//     // Countdown function
-//     const countdownInterval = setInterval(() => {
-//         count--;
-//         if (count > 0) {
-//             btn.textContent = count;
-//         } else {
-//             // Clear the interval when countdown reaches 0
-//             clearInterval(countdownInterval);
-//             // Hide the button
-//             btn.style.display = "none";
-//             // Display "THE Hackathon begins" text
-//             countdownText.textContent = "";
-//             // Trigger confetti animation
-//             makeConfetti();
-//           // Adjust delay as needed
-//         }
-//     }, 1000); // Interval of 1 second (1000 milliseconds)
-// }
+    // Countdown function
+    const countdownInterval = setInterval(() => {
+        count--;
+        if (count > 0) {
+            btn.textContent = count;
+        } else {
+            // Clear the interval when countdown reaches 0
+            clearInterval(countdownInterval);
+            // Hide the button
+            btn.style.display = "none";
+            // Display "THE Hackathon begins" text
+            countdownText.textContent = " - The 30 hours of the Hackathon phase begins...";
+            // Trigger confetti animation
+            makeConfetti();
+          // Adjust delay as needed
+        }
+    }, 1000); // Interval of 1 second (1000 milliseconds)
+}
 
-// function makeConfetti() {
-//   // do this for 30 seconds
-//   var duration = 3* 1000;
-//   var end = Date.now() + duration;
+function makeConfetti() {
+  // do this for 30 seconds
+  var duration = 3* 1000;
+  var end = Date.now() + duration;
 
-//   (function frame() {
+  (function frame() {
    
     
-//     // launch a few confetti from the left edge
-//     confetti({
-//       particleCount: 7,
-//       angle: 60,
-//       spread: 55,
-//       origin: { x: 0, y:1 }
-//     });
-//     // and launch a few from the right edge
-//     confetti({
-//       particleCount: 7,
-//       angle: 120,
-//       spread: 55,
-//       origin: { x: 1, y:1 }
-//     });
+    // launch a few confetti from the left edge
+    confetti({
+      particleCount: 7,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0, y:1 }
+    });
+    // and launch a few from the right edge
+    confetti({
+      particleCount: 7,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1, y:1 }
+    });
 
 
-//     // keep going until we are out of time
-//     if (Date.now() < end) {
-//       requestAnimationFrame(frame);
-//     }
-//   }());
-// }
+    // keep going until we are out of time
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  }());
+}
 
-// btn.addEventListener("click", countdownAndConfetti);
+btn.addEventListener("click", countdownAndConfetti);
 
 
 
